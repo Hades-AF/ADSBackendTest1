2f8a73822d32ff5aa1279a19eeab6309925e879c
Hades <gbalyukw@gmail.com>
2025-05-23 20:57:44 -0400

    Adding SQLDelight Dependencies and Configurations

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.6.20"
+    id("app.cash.sqldelight") version "2.0.1"
 }
 
 group = "org.mbte.mdds.tests"
@@ -17,6 +18,16 @@ dependencies {
     implementation("org.xerial:sqlite-jdbc:3.36.0")
     testImplementation(kotlin("test"))
 	implementation(kotlin("stdlib-jdk8"))
+    implementation("app.cash.sqldelight:sqlite-driver:2.0.1")
+    implementation("app.cash.sqldelight:coroutines-extensions:2.0.1")
+}
+
+sqldelight {
+    databases {
+        create("AddressDatabase") {
+            packageName.set("org.mbte.mdds")
+        }
+    }
 }
 
 tasks.test {
