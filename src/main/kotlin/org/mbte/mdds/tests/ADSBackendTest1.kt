c83f5a04069e8aa0031525c2d3341a06d68f47e8
Hades <gbalyukw@gmail.com>
2025-05-23 21:55:01 -0400

    Updated loadAddressBook and finished step 2

--- src/main/kotlin/org/mbte/mdds/tests/ADSBackendTest1.kt
+++ src/main/kotlin/org/mbte/mdds/tests/ADSBackendTest1.kt
@@ -2,8 +2,10 @@ package org.mbte.mdds.tests
 
 import org.json.JSONObject
 import org.mbte.mdds.util.DatabaseHandler
+import org.mbte.mdds.util.getDirectChildren
 import org.mbte.mdds.util.loadXmlFromFile
 import org.w3c.dom.Document
+import org.w3c.dom.Element
 import java.io.File
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -23,8 +25,14 @@ fun main(args: Array<String>) {
 	// 1. Load the xml file 'ab.xml'
 	val document = test.loadXml("src/main/resources/ab.xml");
 
+	// 2. Load the address book contents from 'ab.xml'
+	val addressBook: AddressBook = document?.let {
+		test.loadAddressBook(document)
+	} ?: run {
+		System.err.println("Failed to load XML, Document is NULL")
+		return
+	}
 	/**
-	 * 2. Load the address book contents from 'ab.xml'
 	 * 3. Insert each contact into the Database
 	 * 4. Retrieve all contacts from the Database
 	 * 5. Convert the contacts from the Database into Json and write to file
@@ -65,8 +73,27 @@ class ADSBackendTest1(): AddressBookInterface {
 		return loadXmlFromFile(File(fileName))
 	}
 
+	// Creating Contact objects based on document elements.
 	override fun loadAddressBook(doc: Document): AddressBook {
-		TODO("Not yet implemented")
+		val contactElements: MutableSet<Element> = getDirectChildren(doc.documentElement)
+		val contacts: MutableList<Contact> = mutableListOf()
+		for (contactElement: Element in contactElements) {
+			contacts.add(Contact(
+				id = contactElement.getElementsByTagName("CustomerID").item(0).textContent,
+				companyName = contactElement.getElementsByTagName("CompanyName").item(0).textContent,
+				name = contactElement.getElementsByTagName("ContactName").item(0).textContent,
+				title = contactElement.getElementsByTagName("ContactTitle").item(0).textContent,
+				address = contactElement.getElementsByTagName("Address").item(0).textContent,
+				city = contactElement.getElementsByTagName("City").item(0).textContent,
+				email = contactElement.getElementsByTagName("Email").item(0).textContent,
+				region = contactElement.getElementsByTagName("Region").item(0)?.textContent,
+				zip = contactElement.getElementsByTagName("PostalCode").item(0)?.textContent,
+				country = contactElement.getElementsByTagName("Country").item(0).textContent,
+				phone = contactElement.getElementsByTagName("Phone").item(0).textContent,
+				fax = contactElement.getElementsByTagName("Fax").item(0)?.textContent
+			))
+		}
+		return AddressBook(contacts)
 	}
 
 	override fun convertToJson(addressBook: AddressBook): JSONObject {
